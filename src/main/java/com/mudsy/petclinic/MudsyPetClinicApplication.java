6de301979cbe4c71defc9dd474dd44c08b5acfce
Marc Mudie <marc.mudie@stannah.co.uk>
2021-11-07 15:16:01 +0000

    Initial commit

---
+++ src/main/java/com/mudsy/petclinic/MudsyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.mudsy.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudsyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudsyPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mudsy/petclinic/MudsyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudsy.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudsyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
